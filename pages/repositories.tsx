fe03590ad71992e275b7bf80706c421cc2ca96ee
fpiola <francis.piola@yahoo.com.br>
2021-02-05 00:13:28 -0300

    repositories

---
+++ pages/repositories.tsx
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import { GetServerSideProps } from 'next'
+import { useEffect, useState } from 'react'
+
+
+type Repo = {
+    name: string;
+    description: string;
+}
+
+type RepositoriesProps = {
+    repos: Repo[]
+}
+
+export default function Repositories({ repos }: RepositoriesProps) {
+
+    return (
+        <div>
+            <h1>Repositories</h1>
+            <ul>
+                {repos.map(repo => (
+                    <li key={repo.name}>{repo.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    const response = await fetch('https://api.github.com/orgs/rocketseat/repos')
+    const data = await response.json()
+    return {
+        props: {
+            repos: data
+        }
+    }
+}

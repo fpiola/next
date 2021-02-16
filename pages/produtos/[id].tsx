d65dcad4baaef4133a94008a8f4e2d8fb4dc6d76
fpiola <francis.piola@yahoo.com.br>
2021-02-16 00:05:21 -0300

    Geração incremental de páginas estáticas

--- pages/produtos/[id].tsx
+++ pages/produtos/[id].tsx
@@ -1,11 +1,19 @@
 
-
-export async function getServerSideProps(context){
-    const id = context.query.id
+export async function getStaticPaths() {
     return {
-        props: {
-            id: id
-        }
+        paths: [
+            { params: { id: '1' } },
+            { params: { id: '2' } }
+        ],
+        fallback: "blocking",
+    }
+}
+
+export async function getStaticProps(context){
+    await delay(5000)
+    const id = context.params.id
+    return {
+        id: id
     }
 }
 
@@ -16,3 +24,7 @@ function Produtos(props) {
 }
 
 export default Produtos;
+
+function delay(ms){
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
